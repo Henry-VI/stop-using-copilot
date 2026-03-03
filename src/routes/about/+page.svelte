ae1227064cf5d978b23eefd35531dae57ab66701
Henry-VI <henryvaniersel@proton.me>
2026-03-03 12:13:55 -0800

    hello alex

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,35 +1,40 @@
 <svelte:head>
   <title>About | Stop Microsoft Copilot!</title>
 </svelte:head>
-<div class="bg-gradient-to-r from-red-300 to-red-400 mt-16 dark:from-red-500 dark:to-red-600 text-center py-12">
+<div class="bg-linear-to-r from-red-300 to-red-400 mt-16 dark:from-red-500 dark:to-red-600 text-center py-12">
   <h1>About Us</h1>
   <div>
     This project is currently controlled by two people, but we're working on finding more volunteers! If you're interesting in helping us, email <a href="mailto:stopcopilot@henryvaniersel.ca">stopcopilot@henryvaniersel.ca</a>.
   </div>
 </div>
-<div class="py-16 bg-gradient-to-r from-amber-400 to-amber-500 dark:from-amber-500 dark:to-orange-600 text-center md:w-1/2 md:mx-auto md:rounded-lg md:my-4" id="goal">
+<div class="py-16 bg-linear-to-r from-amber-400 to-amber-500 dark:from-amber-500 dark:to-orange-600 text-center md:w-1/2 md:mx-auto md:rounded-lg md:my-4" id="goal">
   <h2>Goal</h2>
   Put simply, we're a small group of students focusing on supporting skepticism around AI in our school system. AI is a technology wrapped in a large quanitity of hype and speculation, and we believe that the technology is worth slowing down on to spend more time researching and regulating the industry. See <a href="/">our home page</a> for more details on that.
 </div>
-<div class="bg-gradient-to-r from-green-300 to-green-400 dark:from-green-600 dark:to-green-700 py-8 text-center md:w-2/3 md:mx-auto md:rounded-lg md:my-4" id="team">
+<div class="bg-linear-to-r from-green-300 to-green-400 dark:from-green-600 dark:to-green-700 py-8 text-center md:w-2/3 md:mx-auto md:rounded-lg md:my-4" id="team">
   <h2>Team Members</h2>
-  <div>We're currently a team of 3.</div>
+  <div>We're currently a team of 4.</div>
 </div>
 <hr class="hidden md:block text-neutral-200 dark:text-neutral-500">
 <div class="text-center">
-  <div class="bg-gradient-to-r from-blue-300 to-blue-400 dark:from-blue-500 dark:to-blue-600 py-24 md:w-1/2 md:mx-auto md:rounded-lg md:my-4">
+  <div class="bg-linear-to-r from-blue-300 to-blue-400 dark:from-blue-500 dark:to-blue-600 py-24 md:w-1/2 md:mx-auto md:rounded-lg md:my-4">
     <h2>Henry Van Iersel</h2>
     <div class="text-neutral-600 dark:text-neutral-800"><em>Grade 10, Kitsilano Secondary</em></div>
     <div><b>Roles: Management, Web Development</b></div>
   </div>
-  <div class="bg-gradient-to-r from-indigo-300 to-indigo-400 dark:from-indigo-500 dark:to-indigo-600 py-24 md:w-1/2 md:mx-auto md:rounded-lg md:my-4">
+  <div class="bg-linear-to-r from-indigo-300 to-indigo-400 dark:from-indigo-500 dark:to-indigo-600 py-24 md:w-1/2 md:mx-auto md:rounded-lg md:my-4">
     <h2>Evan Yelizarov Makogon</h2>
     <div class="text-neutral-600 dark:text-neutral-800"><em>Grade 10, Eric Hamber Secondary</em></div>
     <div><b>Roles: Management, Spreading Awareness</b></div>
   </div>
-  <div class="bg-gradient-to-r from-violet-300 to-violet-400 dark:from-violet-500 dark:to-violet-600 py-24 md:w-1/2 md:mx-auto md:rounded-lg md:my-4">
+  <div class="bg-linear-to-r from-violet-300 to-violet-400 dark:from-violet-500 dark:to-violet-600 py-24 md:w-1/2 md:mx-auto md:rounded-lg md:my-4">
     <h2>Peter Leaney</h2>
     <div class="text-neutral-600 dark:text-neutral-800"><em>Grade 12, Kitsilano Secondary</em></div>
     <div><b>Roles: Media Contact/Spreading Awareness</b></div>
+  </div>
+  <div class="bg-linear-to-r from-violet-300 to-violet-400 dark:from-violet-500 dark:to-violet-600 py-24 md:w-1/2 md:mx-auto md:rounded-lg md:my-4">
+    <h2>Alex Chan</h2>
+    <div class="text-neutral-600 dark:text-neutral-800"><em>Grade 10, Eric Hamber Secondary</em></div>
+    <div><b>Roles: TBD</b></div>
   </div>
 </div>
